f02cd2d5189c57ec2de355e7079cc75d52b33eb3
Chris <85393398+Chris9soul@users.noreply.github.com>
2023-04-30 01:39:12 +0300

    add transitions

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
-	import { T, useFrame } from '@threlte/core'
-	import { HTML, Instance, InstancedMesh, OrbitControls, Text } from '@threlte/extras'
+	import { T } from '@threlte/core'
+	import { HTML, Instance, InstancedMesh, OrbitControls, Text, createTransition, transitions } from '@threlte/extras'
+	import { cubicOut } from 'svelte/easing'
 	import { degToRad } from 'three/src/math/MathUtils'
 	import TodoCard, { type Todo } from './TodoCard.svelte'
+	import type { Mesh } from 'three'
 	
+	transitions()
+
+	const scale = createTransition<Mesh>((ref) => {
+    return {
+      tick(t: number) {
+        // t is [0,1] and needs to be converted to [0.5,1]
+        t = 0.5 + t * 0.5
+        ref.scale.set(t, t, t)
+      },
+      easing: cubicOut,
+      duration: 400
+    }
+  })
+
 	let screenSize: number
 	
 	let lastId = 0
@@ -22,7 +38,6 @@
 
 	const deleteTodo = (todoId: number) => {
 		todos = todos.filter((t) => t.id !== todoId)
-
 	}
 
 	function toggleDone(todo: Todo) {
@@ -92,8 +107,8 @@
 <InstancedMesh position.y={-5}>
 	<T.BoxGeometry args={[10, 3, 0.2]} />
 	<T.MeshStandardMaterial color="#fff"/>
-	{#each todos as todo, index}
-		<Instance position={[0, -(3.5 * index), 0]} rotation.y={degToRad(0)}>
+	{#each todos as todo, index (todo.id)}
+		<Instance transition={scale} position={[0, -(3.5 * index), 0]} rotation.y={degToRad(0)}>
 			<HTML transform position={[0, 0.5, 0]}>
 				<TodoCard
 					{todo}
